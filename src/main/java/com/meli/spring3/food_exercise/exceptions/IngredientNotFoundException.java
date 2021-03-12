5d2f20011247000653b3cfa07a356393fae66fa0
Matias Adriel Heimann <matias.heimann@mercadolibre.com>
2021-03-12 14:16:01 -0300

    Revert "spring 3"
    This reverts commit 78ae23220bb3fb494696ec200ebb617499020cd6.

--- src/main/java/com/meli/spring3/food_exercise/exceptions/IngredientNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.meli.spring3.food_exercise.exceptions;
-
-public class IngredientNotFoundException extends RuntimeException{
-
-    public IngredientNotFoundException(String message){
-        super(message);
-    }
-
-}
--- src/main/java/com/meli/spring3/starwars/repositories/StarWarsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.meli.spring3.starwars.repositories;
-
-import com.meli.spring3.starwars.model.CharacterDAO;
-
-import java.util.List;
-
-public interface StarWarsRepository {
-    public List<CharacterDAO> getCharacterIfContainsQuery(String query);
-}
